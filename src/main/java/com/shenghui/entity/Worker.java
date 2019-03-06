a995803ea0a98d6b48b55934af74fc7c76622bcf
chenhc <2803610695@qq.com>
2019-03-06 23:34:11 +0800

    this is dev first commit.

---
+++ src/main/java/com/shenghui/entity/Worker.java
@@ -0,0 +1,13 @@
+package com.shenghui.entity;
+
+public class Worker {
+    private Integer workerId;
+    private String name;
+    private Integer age;
+
+    public Worker(Integer workerId, String name, Integer age) {
+        this.workerId = workerId;
+        this.name = name;
+        this.age = age;
+    }
+}
