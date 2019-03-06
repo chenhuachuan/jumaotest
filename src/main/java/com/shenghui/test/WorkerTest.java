31df8a1eaba37e8cc61d3c387753e62c1a0dfbdb
chenhc <2803610695@qq.com>
2019-03-06 23:47:42 +0800

    this is dev second commit. worktest_println a line

---
+++ src/main/java/com/shenghui/test/WorkerTest.java
@@ -0,0 +1,9 @@
+package com.shenghui.test;
+
+public class WorkerTest {
+
+    public static void main(String[] args) {
+        System.out.println("this is first time test in idea and git");
+    }
+
+}
